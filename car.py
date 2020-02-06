b72ff503830456cbb9a6a666d39f0ae516316ca5
Baurzhan Dosym <baurzhan.zh@gmail.com>
2020-02-05 23:05:00 -0500

    Lesson7 - Unit testing, asserts

---
+++ car.py
@@ -0,0 +1,20 @@
+#Gas type
+#1 Premium
+#2 Regular
+#3 Elite
+#4 Diesel
+class Car:
+    def __init__(self, brand, model, gas_type):
+        self.brand = brand
+        self.model = model
+        self.gas_type = gas_type
+
+    def convert_gas(self):
+        if self.gas_type == 1:
+            return "Premium"
+        elif self.gas_type == 2:
+            return "Regular"
+        elif self.gas_type == 3:
+            return "Elite"
+        else:
+            return "Diesel"
