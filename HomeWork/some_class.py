1d528ddddb2a7f29ee414a8c113ef6a690c5d07b
Gulsina <farakhova.gulsina@gmail.com>
2020-02-06 23:13:52 -0800

    HW

---
+++ HomeWork/some_class.py
@@ -0,0 +1,20 @@
+class SomeClass:
+    def __init__(self, number, string, another_number):
+        self.number=number
+        self.string=string
+        self.another_number=another_number
+
+    def check_odd_or_even_number(self):
+        if self.number%2:
+            return "Odd"
+        else:
+            return "Even"
+
+    def sum_number_anotherNumber(self):
+        return self.number+self.another_number
+
+    def check_CapitalString(self):
+        chk=False
+        if self.string[0].isupper():
+            chk=True
+        return chk
