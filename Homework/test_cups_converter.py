e425ad260773f3a272740e9854b27ed01c885655
Lana <pavlikova.lana@gmail.com>
2020-02-11 18:30:11 -0800

    Merge remote-tracking branch 'remotes/origin/master' into LP77
    # Conflicts:
    #	test_first.py

--- Homework/test_cups_converter.py
+++ Homework/test_cups_converter.py
@@ -1,25 +1,25 @@
 from cup import Cup
 
-def test_cup_oz_convert():
-    # Arrange
-    cup = Cup(2)
-    # Act
-    result = cup.cup_oz_convert()
-    # Assert
-    assert result == 16
-
-def test_cup_tablespoon_convert():
-    # Arrange
-    cup = Cup(3)
-    # Act
-    result = cup.cup_tablespoon_convert()
-    # Assert
-    assert result == 48
-
-def test_cup_gallon_convert():
-    # Arrange
-    cup = Cup(5)
-    # Act
-    result = cup.cup_gallon_convert()
-    # Assert
-    assert result == 1.25
+# def test_cup_oz_convert():
+#     # Arrange
+#     cup = Cup(2)
+#     # Act
+#     result = cup.cup_oz_convert()
+#     # Assert
+#     assert result == 16
+#
+# def test_cup_tablespoon_convert():
+#     # Arrange
+#     cup = Cup(3)
+#     # Act
+#     result = cup.cup_tablespoon_convert()
+#     # Assert
+#     assert result == 48
+#
+# def test_cup_gallon_convert():
+#     # Arrange
+#     cup = Cup(5)
+#     # Act
+#     result = cup.cup_gallon_convert()
+#     # Assert
+#     assert result == 1.25
