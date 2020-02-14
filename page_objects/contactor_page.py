72016b914456a27e9a7c57316a85189ea7cd155e
Lana <pavlikova.lana@gmail.com>
2020-02-14 11:31:20 -0800

    Merge remote-tracking branch 'remotes/origin/master' into LP77
    # Conflicts:
    #	test_first.py

--- page_objects/contactor_page.py
+++ page_objects/contactor_page.py
@@ -11,9 +11,6 @@ class ContractorPage(BasePage):
         return self.wait.until(ec.visibility_of_element_located(
             (By.CSS_SELECTOR, ".ui-multiselect-trigger-icon.ui-clickable.pi.pi-caret-down")))
 
-    def service(self,service):
-        return self.driver.find_element_by_xpath(f"//*[text()='{service}']/preceding-sibling::div")
-
     @property
     def close_dropdown(self):
         return self.driver.find_element_by_css_selector(".ui-multiselect-close.ui-corner-all")
@@ -23,8 +20,11 @@ class ContractorPage(BasePage):
         return self.driver.find_element_by_css_selector(".ui-dropdown-trigger-icon.ui-clickable.pi.pi-caret-down")
 
     @property
-    def n_found(self):
+    def title(self):
         return self.wait.until(ec.visibility_of_any_elements_located((By.CSS_SELECTOR,"h1")))
+
+    def service(self, service):
+        return self.driver.find_element_by_xpath(f"//*[text()='{service}']/preceding-sibling::div")
 
     def state(self,state):
         return self.driver.find_element_by_xpath(f"//span[text()='{state}']")
@@ -36,12 +36,21 @@ class ContractorPage(BasePage):
     def choose_contractor(self, service,state,county=""):
         self.dropdown_service_type.click()
         self.service(service).click()
-        self.close_dropdown.click()
+        self.dropdown_service_close.click()
         self.state_dropdown.click()
         self.state(state).click()
 
-    def is_not_found_message(self):
-        return len(self.n_found) == 1
+    def is_title_exist(self):
+        return len(self.title) == 1
+
+    def is_status_exist(self):
+        return len(self.status) == 1
+
+    def is_originated_by_exist(self):
+        return len(self.title) == 1
+
+    def is_r_letter_exist(self):
+        return len(self.title) == 1
 
 
 
